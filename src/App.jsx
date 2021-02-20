4af86444862ede01e982327245d8079c3ee413c1
Chris Burton <chris@fullstackcaveman.com>
2021-02-20 14:49:10 -0700

    Refactored state structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import { useState, useEffect } from 'react';
+import ReactPaginate from 'react-paginate';
 import axios from 'axios';
 import { AKABAB_BASE_URL } from './constants';
 import './App.css';
 import Characters from './components/Characters';
 
 const App = () => {
-	const [characters, setCharacters] = useState([]);
-	const [fetchCharacters, setFetchCharacters] = useState('all.json');
-	const [fetchCharacter, setFetchCharacter] = useState(1);
-	const [page, setPage] = useState(3);
+	const [characterData, setCharacterData] = useState({
+		characters: [],
+		fetchCharacters: 'all.json',
+		fetchCharacter: 1,
+		page: 3,
+	});
+	// const [characters, setCharacters] = useState([]);
+	// const [fetchCharacters, setFetchCharacters] = useState('all.json');
+	// const [fetchCharacter, setFetchCharacter] = useState(1);
+	// const [page, setPage] = useState(3);
 
 	useEffect(() => {
 		const getCharacters = () => {
 			axios
-				.get(`${AKABAB_BASE_URL}/${fetchCharacters}`)
+				.get(`${AKABAB_BASE_URL}/${characterData.fetchCharacters}`)
 				.then((res) => {
 					console.log(res.data);
-					setCharacters(res.data);
+					setCharacterData({ characters: res.data });
 				})
 				.catch((err) => {
 					console.log(err);
@@ -26,22 +33,22 @@ const App = () => {
 		getCharacters();
 	}, []);
 
-	const setNextPage = () => {
-		setPage(page + 1);
-	};
+	// const setNextPage = () => {
+	// 	setPage(page + 1);
+	// };
 
-	const setPrevPage = () => {
-		setPage(page - 1);
-	};
+	// const setPrevPage = () => {
+	// 	setPage(page - 1);
+	// };
 
 	return (
 		<div className='App'>
 			<h1 className='Header'>Star Wars Characters</h1>
 			<Characters
-				characters={characters}
-				page={page}
-				nextpage={setNextPage}
-				prevpage={setPrevPage}
+				characters={characterData.characters}
+				page={characterData.page}
+				// nextpage={setNextPage}
+				// prevpage={setPrevPage}
 			/>
 		</div>
 	);
