48a2ad889d6112f4f1fc6ff85c188fe376362f55
Chris Burton <chris@fullstackcaveman.com>
2021-02-21 15:46:20 -0700

    Rest App component to be main page. Moved Characters to its own component. Removed unused dependencies

---
+++ src/components/CharacterPage.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { BASE_URL } from '../constants';
+import Characters from './Characters';
+import Pagination from './Pagination';
+
+const CharacterPage = () => {
+	const pageOne = document.getElementById('page 1');
+	const [characters, setCharacters] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [charactersPerPage] = useState(10);
+
+	const pages = document.querySelectorAll('.page-item');
+
+	// Sets pagination item1 to active
+	window.onload = () => {
+		pageOne.classList.add('active');
+	};
+
+	// Gets characters from the api
+	useEffect(() => {
+		const fetchCharacters = () => {
+			setLoading(true);
+			axios
+				.get(`${BASE_URL}/all.json`)
+				.then((res) => {
+					setCharacters(res.data);
+					setLoading(false);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		};
+		fetchCharacters();
+	}, []);
+
+	// Sets structure of pagination
+	const indexOfLastCharacter = currentPage * charactersPerPage;
+	const indexOfFirstCharacter = indexOfLastCharacter - charactersPerPage;
+	const currentCharacters = characters.slice(
+		indexOfFirstCharacter,
+		indexOfLastCharacter
+	);
+
+	// Controls which characters to display and button styling
+	const paginate = (pageNumber) => {
+		setCurrentPage(pageNumber);
+		console.log(pages);
+		// Clears active class from all pagination items
+		pages.forEach((page) => {
+			page.classList.remove('active');
+		});
+
+		// Adds class 'active' to pagination element
+		pages.forEach((page) => {
+			if (page.id.match(`page ${pageNumber}`)) {
+				page.classList.add('active');
+			}
+		});
+	};
+
+	return (
+		<div className='CharacterPage'>
+			<Characters characters={currentCharacters} loading={loading} />
+			<Pagination
+				charactersPerPage={charactersPerPage}
+				totalCharacters={characters.length}
+				paginate={paginate}
+			/>
+		</div>
+	);
+};
+
+export default CharacterPage;
