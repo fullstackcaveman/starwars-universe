9c30c6dd5616e90d4b17e7e2bdf4693f4afd2cf0
Chris Burton <chris@fullstackcaveman.com>
2021-02-21 16:53:00 -0700

    Added star wars font and det up _variables to use it

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Switch, Route } from 'react-router-dom';
-
+import './fonts/STARJEDI.TTF';
 import CharacterPage from './components/characters/CharacterPage';
 import Home from './components/Home';
 
