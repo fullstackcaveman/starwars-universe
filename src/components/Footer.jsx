ab0cd7ef1177fbb055c086fcebe60f6696268335
Chris Burton <chris@fullstackcaveman.com>
2021-03-06 18:11:21 -0700

    Footer setup

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer>
+			<div className='footer-container'>
+				<div className='footer-top'>
+					<h2>Footer Works</h2>
+					<h2>Other Stuff</h2>
+				</div>
+				<div className='footer-bottom'>
+					<h2>
+						Star Wars and all associated names and/or images are &copy;Lucasfilm
+						Ltd. Images were freely collected from Wookiepedia or Licensed
+						through Adobe Stock.
+					</h2>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
