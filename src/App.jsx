69ed0d2d951eae003b775897448069e03e6c041b
Chris Burton <chris@fullstackcaveman.com>
2021-03-11 20:54:55 -0700

    Remove lazy load from routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,32 @@
-import { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Loader from './components/Loader';
-
-// Lazy load components
-const CharacterInfo = lazy(() =>
-	import('./components/characters/CharacterInfo')
-);
-const CharacterPage = lazy(() =>
-	import('./components/characters/CharacterPage')
-);
-const Films = lazy(() => import('./components/films/Films'));
-const Footer = lazy(() => import('./components/Footer'));
-const Header = lazy(() => import('./components/Header'));
-const HeroSection = lazy(() => import('./components/HeroSection'));
-const Home = lazy(() => import('./components/Home'));
-const Planets = lazy(() => import('./components/planets/Planets'));
-const Species = lazy(() => import('./components/species/Species'));
-const Starships = lazy(() => import('./components/starships/Starships'));
-const Vehicles = lazy(() => import('./components/vehicles/Vehicles'));
+import CharacterInfo from './components/characters/CharacterInfo';
+import CharacterPage from './components/characters/CharacterPage';
+import Films from './components/films/Films';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import Home from './components/Home';
+import Planets from './components/planets/Planets';
+import Species from './components/species/Species';
+import Starships from './components/starships/Starships';
+import Vehicles from './components/vehicles/Vehicles';
 
 const App = () => {
 	return (
 		<div className='App'>
-			<Suspense fallback={Loader}>
-				<Header />
-				<HeroSection />
-				<Switch>
-					<Route exact path='/characters' component={CharacterPage} />
-					<Route path='/characters/:id' component={CharacterInfo} />
-					<Route exact path='/films' component={Films} />
-					<Route path='/planets' component={Planets} />
-					<Route exact path='/species' component={Species} />
-					<Route path='/starships' component={Starships} />
-					<Route exact path='/vehicles' component={Vehicles} />
-
-					<Route exact path='/' component={Home} />
-				</Switch>
-				<Footer />
-			</Suspense>
+			<Header />
+			<HeroSection />
+			<Switch>
+				<Route exact path='/characters' component={CharacterPage} />
+				<Route path='/characters/:id' component={CharacterInfo} />
+				<Route exact path='/films' component={Films} />
+				<Route path='/planets' component={Planets} />
+				<Route exact path='/species' component={Species} />
+				<Route path='/starships' component={Starships} />
+				<Route exact path='/vehicles' component={Vehicles} />
+				<Route exact path='/' component={Home} />
+			</Switch>
+			<Footer />
 		</div>
 	);
 };
